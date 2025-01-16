e10c9c58d9960298838978800a0d87af191ab1e5
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-01-16 18:50:22 +0000

    who y action

--- src/app.js
+++ src/app.js
@@ -8,4 +8,18 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird", "the cat"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let frase = quien + " " + accion;
+  console.log(frase);
 };
